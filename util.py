6873b3db1a80c94e3eabba6dfc0f29e41b5e440a
Julian Schrittwieser <j.schrittwieser@gmail.com>
2022-02-07 21:09:39 +0000

    preserve whitespace

--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import numpy as np
 import polars as pl
-from tabulate import tabulate
+import tabulate
+
+tabulate.PRESERVE_WHITESPACE = True
 
 def line_hist(data, bins=16): 
     bars = " ▁▂▃▄▅▆▇█"
@@ -28,8 +30,8 @@ def summarize(df: pl.DataFrame):
         
     summaries = metadata + [(n, vs.row(0)) for n, vs in pl_summaries]
     summaries.append(("histogram", [line_hist(df[c].drop_nulls()) for c in df.columns]))
-        
-    return f"pl.DataFrame of shape {df.shape}\n\n" + tabulate(dict(summaries), headers='keys', tablefmt="fancy_grid")
+    summary = tabulate.tabulate(dict(summaries), headers='keys', tablefmt="fancy_grid")
+    return f"pl.DataFrame of shape {df.shape}\n\n" + summary
 
 def covariance(df: pl.DataFrame):
     cov = np.cov(np.stack([df[c] for c in df.columns], axis=0)) 
